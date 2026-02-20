f701bae1990b33fd8d0cd0e674cc794a6f495191
Matheus Bezerra Domingos <168386167+matheusb-dev@users.noreply.github.com>
2026-02-20 16:09:33 -0300

    Ultimo que faltava

---
+++ ex33.cs
@@ -0,0 +1,39 @@
+﻿/** NOME: Matheus Bezerra Domingos
+    RA: 166479-2024
+    
+    NOME: Otávio Augusto Zangelmi Costa
+    RA: 159752-2023
+
+    NOME: Luiz Eduardo Marques da Silva 
+    RA:168685-2024
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EX_C__1_15
+{
+    internal class ex33
+    {
+        public static void EX33()
+        {
+            int bas;
+            int altura;
+            int resultado;
+
+            Console.WriteLine("Calcule a area de um retangulo");
+
+            Console.WriteLine("Digite a Base: ");
+            bas = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite a Altura: ");
+            altura = Convert.ToInt32(Console.ReadLine());
+
+            resultado = bas * altura;
+
+            Console.WriteLine("Resultado: " + resultado);
+
+        }
+    }
+}
